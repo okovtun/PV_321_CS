52134f2ac5f0a45d3ae959e46ded5b363d0c9d69
Clayman <Clayman@DESKTOP-DM8E77U>
2023-10-27 20:25:27 +0300

    Calc DONE

--- Introduction/Calc/Program.cs
+++ Introduction/Calc/Program.cs
@@ -10,6 +10,23 @@ namespace Calc
 	{
 		static void Main(string[] args)
 		{
+			Console.Write("Введите выражение: ");
+			string expression = Console.ReadLine();
+			//Console.WriteLine(expression);
+			expression = expression.Replace(",", ".");
+			//Console.WriteLine(expression);
+			string[] values = expression.Split('+', '-', '*', '/');
+			//for (int i = 0; i < values.Length; i++) Console.Write(values[i] + "\t");
+			//Console.WriteLine();
+			double a = Convert.ToDouble(values[0]);
+			double b = Convert.ToDouble(values[1]);
+			//Console.WriteLine($"a = {a}");
+			//Console.WriteLine($"b = {b}");
+			if (expression.Contains('+')) Console.WriteLine($"{a} + {b} = {a + b}");
+			else if (expression.Contains('-')) Console.WriteLine($"{a} - {b} = {a - b}");
+			else if (expression.Contains('*')) Console.WriteLine($"{a} * {b} = {a * b}");
+			else if (expression.Contains('/')) Console.WriteLine($"{a} / {b} = {a / b}");
+			else Console.WriteLine("Нэт такой животный...");
 		}
 	}
 }
